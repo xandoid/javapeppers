34b773df5adcd3344e31cb5619049fcdd8638ce2
Gary Anderson <xandoid@gmail.com>
2016-12-20 15:56:53 -0800

    Testing git interaction

---
+++ src/com/codeondemand/javapeppers/aleppo/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+* The com.codeondemand.javapeppers.aleppo.common package contains some of the common code for use by
+* the other aleppo packages, including RecordProcessor, RecordCapsule, and DataCapsule
+* 
+*@author xandoid@gmail.com  
+*/
+package com.codeondemand.javapeppers.aleppo.common;
